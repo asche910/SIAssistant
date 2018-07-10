0402e658a74218b47de17e43ef3a8fc47cb0e657
apknet <apknet@163.com>
2018-07-10 14:08:45 +0800

    Upload FragmentNew

--- app/src/main/java/com/sia/siassistant/FragmentNew.java
+++ app/src/main/java/com/sia/siassistant/FragmentNew.java
@@ -1,15 +1,79 @@
 package com.sia.siassistant;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class FragmentNew extends Fragment {
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static com.sia.siassistant.WheelView.TAG;
+
+public class FragmentNew extends Fragment  implements View.OnClickListener{
+
+
+    private final String[] PLANETS = new String[]{"3天", "7天", "21天", "30天", "100天", "365天", "永久"};
+    private int dayIndex = -1;
+    Button btnSug;
+    View view;
+    WheelView wheelView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment_new, container, false );
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        btnSug = getActivity().findViewById(R.id.btn_add_sug);
+        btnSug.setOnClickListener(this);
+
+        view = LayoutInflater.from(getActivity()).inflate(R.layout.wheelview, null);
+        wheelView = view.findViewById(R.id.wheelView);
+        wheelView.setOffset(2);
+        wheelView.setItems(Arrays.asList(PLANETS));
+        wheelView.setSeletion(3);
+        wheelView.setOnWheelViewListener(new WheelView.OnWheelViewListener(){
+            @Override
+            public void onSelected(int selectedIndex, String item) {
+                super.onSelected(selectedIndex, item);
+                selectedIndex -= 2;
+                dayIndex = selectedIndex;
+                Log.d(TAG, "[Dialog]selectedIndex: " + selectedIndex + ", item: " + item);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_add_sug:
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("选择天数")
+                        .setView(view)
+                        .setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dayIndex = -1;
+                            }
+                        })
+                        .show();
+                break;
+        }
     }
 }
