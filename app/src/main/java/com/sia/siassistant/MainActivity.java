624855bd858ebd96e67098a4fc50f3b393a2f591
TuAo <1072459221@qq.com>
2018-07-10 15:19:14 +0800

    tuao

--- app/src/main/java/com/sia/siassistant/MainActivity.java
+++ app/src/main/java/com/sia/siassistant/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sia.siassistant;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v4.app.Fragment;
@@ -7,6 +8,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -30,7 +32,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         init();
 
         //彩色33B5E5
